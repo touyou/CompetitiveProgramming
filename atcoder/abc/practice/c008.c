5b47cde3454224c50edf05b3c7ece6c5ed1fd376
touyou <465697+touyou@users.noreply.github.com>
2018-08-08 00:09:32 +0900

    add c challenge (aug 7)

--- atcoder/abc/practice/c008.c
+++ atcoder/abc/practice/c008.c
@@ -15,43 +15,11 @@ int main() {
             if (c[i] % c[j] == 0) cnt[i]++;
         }
     }
-    ll sumall = 1;
-    for (ll i = 1; i <= n; i++) sumall *= i;
-    //for (int i = 0; i < n; i++) {
-    //    int cnt = 1;
-    //    if (c[i] == -1) continue;
-    //    for (int j = i + 1; j < n; j++) {
-    //        if (c[i] == c[j]) {
-    //            cnt++;
-    //            c[j] = -1;
-    //        }
-    //    }
-    //    c[i] = cnt;
-    //    sumall /= table[cnt];
-    //}
-    ll ret = 0;
+    double ret = 0;
     for (int i = 0; i < n; i++) {
-        //if (c[i] == -1) continue;
-        if (cnt[i] == 0) {
-            ret += sumall;
-            continue;
-        }
-        for (int r = 0; r <= cnt[i]; r+=2) {
-            for (int l = 0; l < n - cnt[i]; l++) {
-                ll tmp = 1;
-                if (r != 0) tmp *= table[cnt[i]] / table[r];
-                if (l != 0) tmp *= table[n - cnt[i] - 1] / table[l];
-                tmp *= table[r + l] * table[n - r - l - 1];
-                //for (int j = 0; j < n; j++) {
-                //    if (c[j] == -1) continue;
-                //    if (i == j) tmp /= table[c[j]-1];
-                //    else tmp /= table[c[j]];
-                //}
-                ret += tmp;
-            }
-        }
-        printf("%lld %lld %d\n", sumall, ret, i);
+        if (cnt[i] % 2 == 1) ret += 0.5;
+        else ret += ((double)cnt[i] + 2.0) / (2.0 * (double)cnt[i] + 2.0);
     }
-    printf("%lf\n", (double)ret/(double)sumall);
+    printf("%lf\n", ret);
     return 0;
 }
