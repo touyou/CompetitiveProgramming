01e6df384dfd1e7b77f1f0ecb46c7c5a4cf2f7a1
touyou <fujiyou1121@gmail.com>
2018-08-11 23:20:54 +0900

    complete abc105

--- atcoder/abc/105/c.cpp
+++ atcoder/abc/105/c.cpp
@@ -5,8 +5,21 @@ using ll = long long;
 int main() {
     int n; scanf("%d", &n);
     string s = "";
-    for (int i = 34; i >= 0; i--) {
-        
+    ll x = 2;
+    int i = 0;
+    if (n == 0) {
+        puts("0");
+        return 0;
+    }
+    while (n != 0) {
+        if (n % x != 0) {
+            s = "1" + s;
+            n -= (x / 2) * (i % 2 == 0 ? 1 : -1);
+        } else {
+            s = "0" + s;
+        }
+        x *= 2;
+        i++;
     }
     cout << s << endl;
 }
