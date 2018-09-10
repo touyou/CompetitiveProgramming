9d0333b13ebf2313cac3d77a488f22a15b7f6939
touyou <fujiyou1121@gmail.com>
2018-09-10 23:43:08 +0900

    q20 doing

---
+++ cpaw/crypto20.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string ciph = "ruoYced_ehpigniriks_i_llrg_stae";
+    for (int key = 1; key < ciph.length(); key++) {
+//        for (int i = key - 1; i <= ciph.length(); i++) {
+//            for (int j = i; j >= i - key + 1; j--) printf("%c", ciph[j]);
+//        }
+//        puts("");
+       
+    }
+}
