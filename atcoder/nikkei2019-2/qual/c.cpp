fbc0409620f355fc573622a59f50a00ec7c52698
touyou <465697+touyou@users.noreply.github.com>
2019-11-09 23:07:15 +0900

    nikkei2019-2

--- atcoder/nikkei2019-2/qual/c.cpp
+++ atcoder/nikkei2019-2/qual/c.cpp
@@ -1,3 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {}
+int n;
+int a[100000], b[100000];
+int main() {
+  scanf("%d", &n);
+  for (auto i = 0; i < n; i++) scanf("%d", a + i);
+  for (auto i = 0; i < n; i++) scanf("%d", b + i);
+  sort(a, a + n);
+  sort(b, b + n);
+  bool flag = true;
+  for (auto i = 0; i < n; i++) {
+    if (a[i] > b[i]) flag = false;
+  }
+  puts(flag ? "Yes" : "No");
+}
