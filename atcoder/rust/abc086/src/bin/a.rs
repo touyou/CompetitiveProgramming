4eadeb8f3e078cae14ad1048231659f71716873d
touyou <465697+touyou@users.noreply.github.com>
2026-02-07 16:14:23 +0900

    update

--- atcoder/rust/abc086/src/bin/a.rs
+++ atcoder/rust/abc086/src/bin/a.rs
@@ -3,7 +3,7 @@ use proconio::*;
 fn main() {
     input! {
         a: u32,
-        b: u32
+    b: u32
     };
     let ans = if (a * b) % 2 == 0 { "Even" } else { "Odd" };
     println!("{}", ans);
